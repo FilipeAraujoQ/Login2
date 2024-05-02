326ff0e83d1250ec4377b3fac9951331ab9ff14f
Filipe Araujo Queiroz <146143459+FilipeAraujoQ@users.noreply.github.com>
2024-05-02 09:38:14 -0300

    Add files via upload

---
+++ assets/js/singup.js
@@ -0,0 +1,98 @@
+//DARK MODE
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () => {
+    const form = document.getElementById('login_form');
+
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon'); //trocar o icone da lua
+        mode.classList.add('fa-sun');
+
+        form.classList.add('dark'); //aqui é a magica
+        return ;
+    }
+    
+    mode.classList.remove('fa-sun');
+    mode.classList.add('fa-moon');
+
+    form.classList.remove('dark');
+});
+
+//CODIGO
+let btn = document.querySelector('#verSenha')
+let btnConfirm = document.querySelector('#verConfirmSenha')
+//falta implementar isso
+
+let nome = document.querySelector('#name')
+//let labelNome = document.querySelector('#labelNome')
+let validNome = false
+
+let usuario = document.querySelector('#email')
+let labelUsuario = document.querySelector('#labelEmail')
+let validUsuario = false
+
+let senha = document.querySelector('#senha')
+let labelSenha = document.querySelector('#labelSenha')
+let validSenha = false
+
+let confirmSenha = document.querySelector('#confirmSenha')
+let labelConfirmSenha = document.querySelector('#labelConfirmSenha')
+let validConfirmSenha = false
+
+let msgError = document.querySelector('#msgError')
+let msgSuccess = document.querySelector('#msgSuccess')
+
+
+
+function cadastrar(){
+  if(validNome && validUsuario && validSenha && validConfirmSenha){
+    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
+    
+    listaUser.push(
+    {
+      nomeCad: nome.value,
+      userCad: usuario.value,
+      senhaCad: senha.value
+    }
+    )
+    
+    localStorage.setItem('listaUser', JSON.stringify(listaUser))
+    
+   
+    msgSuccess.setAttribute('style', 'display: block')
+    msgSuccess.innerHTML = '<strong>Cadastrando usuário...</strong>'
+    msgError.setAttribute('style', 'display: none')
+    msgError.innerHTML = ''
+    
+    setTimeout(()=>{
+        window.location.href = '../html/signin.html'
+    }, 3000)
+  
+    
+  } else {
+    msgError.setAttribute('style', 'display: block')
+    msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar</strong>'
+    msgSuccess.innerHTML = ''
+    msgSuccess.setAttribute('style', 'display: none')
+  }
+}
+
+btn.addEventListener('click', ()=>{
+  let inputSenha = document.querySelector('#senha')
+  
+  if(inputSenha.getAttribute('type') == 'password'){
+    inputSenha.setAttribute('type', 'text')
+  } else {
+    inputSenha.setAttribute('type', 'password')
+  }
+})
+
+btnConfirm.addEventListener('click', ()=>{
+  let inputConfirmSenha = document.querySelector('#confirmSenha')
+  
+  if(inputConfirmSenha.getAttribute('type') == 'password'){
+    inputConfirmSenha.setAttribute('type', 'text')
+  } else {
+    inputConfirmSenha.setAttribute('type', 'password')
+  }
+})
